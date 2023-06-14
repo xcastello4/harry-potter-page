4e5fcdaec2ff9819fe32c3ba3c6966cb2d895232
Xavier Castello <xavier.castello@seidor.es>
2023-06-14 13:29:04 +0200

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: 'class',
+	content: [
+		'./src/pages/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./src/layouts/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
