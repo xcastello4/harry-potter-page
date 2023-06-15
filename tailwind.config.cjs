fb0fc78fe21f44396501022a69c1738a8601de41
Xavier Castello <xavier.castello@seidor.es>
2023-06-15 10:27:50 +0200

    add components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
 	darkMode: 'class',
 	content: [
 		'./src/pages/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./src/components/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 		'./src/layouts/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 	],
 	theme: {
